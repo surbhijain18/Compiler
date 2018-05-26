976a950d3fda0ef329dac88c59adf6da288cf027
surbhijain18 <34788470+surbhijain18@users.noreply.github.com>
2018-05-25 21:15:26 -0400

    Update RuntimeLog.java

--- RuntimeLog.java
+++ RuntimeLog.java
@@ -1,22 +1,3 @@
-/**
- * 
- * A simple globalLog that can be used to record a trace of 
- * an instrumented program.
- * 
- * The output can be used for grading and debugging.
- *
- * This code is used in the class project in COP5556 Programming Language Principles 
- * at the University of Florida, Spring 2018.
- * 
- * This software is solely for the educational benefit of students 
- * enrolled in the course during the Spring 2018 semester.  
- * 
- * This software, and any software derived from it,  may not be shared with others or posted to public web sites,
- * either during the course or afterwards.
- * 
- *  @Beverly A. Sanders, 2018
- */
-
 
 package cop5556sp18;
 
